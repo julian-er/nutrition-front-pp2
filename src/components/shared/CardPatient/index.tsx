2e6150b733c4b052585cf5556452134788752641
Julián Rosalen <rosalenjulian@gmail.com>
2022-10-11 21:06:19 -0300

    patients profile image

--- src/components/shared/CardPatient/index.tsx
+++ src/components/shared/CardPatient/index.tsx
@@ -1,25 +1,27 @@
-import React, { PropsWithChildren } from 'react'
+import React, { PropsWithChildren } from 'react';
+import { profileIcon } from '../../../shared/icons';
 import styles from './Card.module.scss';
 
 export interface ICard {
-    name: string,
-    age:string,
-    callback:any,
-    profile_image:string
-  }
-  
+  name: string;
+  age: string;
+  callback: any;
+  profile_image: string;
+}
 
 function Card(props: PropsWithChildren<ICard>) {
-    const{name,age,profile_image,callback}=props
+  const { name, age, profile_image, callback } = props;
   return (
-  <button className={styles.card_wrapper} onClick={callback}>
-        <img className={styles.patient_image} src={profile_image} alt="user" />
-        <div className={styles.patient_info}>
-          <h3><b>{name}</b></h3>
-          <p>Age:{age}</p>
-    </div>
-  </button>
-  )
+    <button className={styles.card_wrapper} onClick={callback}>
+      <div className={styles.profileImage}>{profile_image ? <img className={styles.patient_image} src={profile_image} alt="user" /> : <>{profileIcon}</>}</div>
+      <div className={styles.patient_info}>
+        <h3>
+          <b>{name}</b>
+        </h3>
+        <p>Age:{age}</p>
+      </div>
+    </button>
+  );
 }
 
 export default Card;
