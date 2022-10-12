ce59bf22955f9dee06c38073a148ac2f535b3bdb
gobu-github <hgobulin.ds@gmail.com>
2022-10-11 22:15:24 -0300

    Add style on input search (allergy(

--- src/pages/Allergies/index.tsx
+++ src/pages/Allergies/index.tsx
@@ -35,8 +35,7 @@ function Allergies() {
         <header className={styles.headerWrapper}>
           <h2>Allergies</h2>
           <div className={styles.searchWrapper}>
-            <p>Search</p>
-            <input type={'search'} onChange={handleSetSearch} />
+            <input className={styles.searchWrapper_input} type={'search'} placeholder="search" onChange={handleSetSearch} />
           </div>
         </header>
         <div className={styles.content}>
